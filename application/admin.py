7eb2050fb4f3d77557816f6334025ba68142429d
AshHvostovsky <ash.stdw@gmail.com>
2024-10-13 15:55:10 +0800

    Added student and group models

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from application.models import Student, Group
+
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['fio', 'record_number', 'group']
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "form_of_education"]
--- application/models.py
+++ application/models.py
@@ -2,7 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    fio = models.TextField() # Student's name
-    record_number = models.TextField()
+    fio = models.TextField("ФИО") # Student's name
+    record_number = models.TextField("Номер зачетки")
 
-    group_name = models.TextField()
+    # group_name = models.TextField("Группа")
+
+    group = models.ForeignKey("Group", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+    def __str__(self) -> str:
+        return self.fio
+
+class Group(models.Model):
+    name = models.TextField("Название")
+    form_of_education = models.TextField("Форма обучения")
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self) -> str:
+        return self.name
