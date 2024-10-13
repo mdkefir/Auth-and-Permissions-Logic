ab4c6b889e095226743c9e9880bd65578a8a8be3
dmitrii.kalenitskii <dogee4803@gmail.com>
2024-10-13 21:51:52 +0800

    Change DB to PostgreSQL Also fixed mistake in admin.py

--- application/admin.py
+++ application/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from application.models import Student, Group
+from application.models import Student, Group, Speciality, Subject, Teacher
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'patronymic', 'service_number']
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['fio', 'record_number', 'group']
+    list_display = ['last_name', 'first_name', 'patronymic', 'record_number', 'group']
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "form_of_education"]
+    list_display = ['name', 'group_number', 'form_of_education', 'speciality']
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name']
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'teacher']
