33d86a225636c29125be8c4fe48a597871e84b7f
AshHvostovsky <ash.stdw@gmail.com>
2024-11-17 16:39:50 +0800

    Added models

--- application/admin.py
+++ application/admin.py
@@ -1,23 +1,95 @@
 from django.contrib import admin
 
-from application.models import Student, Group, Speciality, Subject, Teacher
+from .models import (
+    Teacher, Student, Group, Disciple, Attendance, Course_project, Diploma,
+    Education_plan, Form_control, Grade, Hours_per_semestr, Nagruzka, Practise,
+    Practise_type, Rating, Rating_type, Speciality
+)
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ['last_name', 'first_name', 'patronymic', 'service_number']
+    list_display = ['name']
+    search_fields = ['name']
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['last_name', 'first_name', 'patronymic', 'record_number', 'group']
+    list_display = ['name', 'birth_date', 'sex', 'school', 'entery_score', 'group', 'rating']
+    list_filter = ['sex', 'group', 'rating']
+    search_fields = ['name', 'school']
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ['name', 'group_number', 'form_of_education', 'speciality']
+    list_display = ['code', 'title']
+    search_fields = ['code', 'title']
+
+@admin.register(Disciple)
+class DiscipleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['date_time', 'student', 'nagruzka']
+    list_filter = ['date_time']
+    search_fields = ['student__name']
+
+@admin.register(Course_project)
+class CourseProjectAdmin(admin.ModelAdmin):
+    list_display = ['grade', 'student', 'hps']
+    search_fields = ['student__name']
+
+@admin.register(Diploma)
+class DiplomaAdmin(admin.ModelAdmin):
+    list_display = ['grade', 'student', 'teacher', 'plan']
+    search_fields = ['student__name', 'teacher__name']
+
+@admin.register(Education_plan)
+class EducationPlanAdmin(admin.ModelAdmin):
+    list_display = ['code', 'year_of_conclude']
+    search_fields = ['code']
+
+@admin.register(Form_control)
+class FormControlAdmin(admin.ModelAdmin):
+    list_display = ['form', 'hps']
+    search_fields = ['form']
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ['grade', 'student', 'form_control']
+    search_fields = ['student__name']
+
+@admin.register(Hours_per_semestr)
+class HoursPerSemesterAdmin(admin.ModelAdmin):
+    list_display = ['hours', 'semester', 'plan', 'disciple']
+    search_fields = ['disciple__name', 'plan__code']
+
+@admin.register(Nagruzka)
+class NagruzkaAdmin(admin.ModelAdmin):
+    list_display = ['teacher', 'group', 'form_control']
+    search_fields = ['teacher__name', 'group__title']
+
+@admin.register(Practise)
+class PractiseAdmin(admin.ModelAdmin):
+    list_display = ['grade', 'student', 'education_plan', 'practise_type']
+    search_fields = ['student__name', 'practise_type__title']
+
+@admin.register(Practise_type)
+class PractiseTypeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['event', 'score', 'rating_type']
+    search_fields = ['event', 'rating_type__title']
+
+@admin.register(Rating_type)
+class RatingTypeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
 
 @admin.register(Speciality)
 class SpecialityAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name']
+    list_display = ['code', 'title']
+    search_fields = ['title', 'code']
 
-@admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name', 'teacher']
--- application/models.py
+++ application/models.py
@@ -2,100 +2,219 @@ from django.db import models
 
 
 class Teacher(models.Model):
-    last_name = models.TextField(max_length=30, verbose_name="Фамилия")
-    first_name = models.TextField(max_length=30, verbose_name="Имя")
-    patronymic = models.TextField(max_length=30, verbose_name="Отчество")
-
-    # NOTE Возможно не пригодится
-    service_number = models.TextField(max_length=15, verbose_name="Служебный номер")
-
+    name = models.TextField(max_length=255, verbose_name="ФИО")
+    
     class Meta:
         verbose_name = "Преподаватель"
         verbose_name_plural = "Преподаватели"
 
     def __str__(self) -> str:
-        return f"{self.last_name} {self.first_name} {self.patronymic}"
+        return f"{self.name}"
 
 
 class Student(models.Model):
-    last_name = models.TextField(max_length=30, verbose_name="Фамилия")
-    first_name = models.TextField(max_length=30, verbose_name="Имя")
-    patronymic = models.TextField(max_length=30, verbose_name="Отчество")
-
-    record_number = models.TextField(max_length=8, verbose_name="Номер зачетки")
-
-    group = models.ForeignKey("Group", on_delete=models.CASCADE, null=True)
+    name = models.TextField(max_length=255, verbose_name="ФИО")
+    birth_date = models.DateField(verbose_name="Дата рождения", null=True, blank=True)
+    sex = models.CharField(max_length=1, verbose_name="Пол") # F - female, M - male
+    school = models.TextField(max_length=255, verbose_name="Школа")
+    entery_score = models.IntegerField(verbose_name="Средний бал") # ПОЧЕМУ ОН ЦЕЛЫЙ В БД? ИЛИ ЭТО НЕ БАЛ
+    
+    group = models.ForeignKey("Group", on_delete=models.SET_NULL, null=True)
+    rating = models.ForeignKey("Rating", on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name = "Студент"
         verbose_name_plural = "Студенты"
 
     def __str__(self) -> str:
-        return f"{self.last_name} {self.first_name} {self.patronymic}"
+        return f"{self.name} {self.birth_date} {self.sex} {self.school} {self.entery_score} {self.group} {self.rating}"
 
 
 class Group(models.Model):
-    name = models.TextField(max_length=30, verbose_name="Название группы")
-    group_number = models.IntegerField(verbose_name="Номер группы")
-    form_of_education = models.TextField(max_length=15, verbose_name="Форма обучения")
-
-    speciality = models.ForeignKey("Speciality", on_delete=models.SET_NULL, null=True)
+    title = models.TextField(max_length=255, verbose_name="Название группы")
+    code = models.TextField(max_length=255, verbose_name="Код группы")
 
     class Meta:
         verbose_name = "Группа"
         verbose_name_plural = "Группы"
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.code} {self.title}"
+
+class Disciple(models.Model):
+    name = models.TextField(max_length=255, verbose_name="Название дисциплины")
+
+    class Meta:
+        verbose_name = "Дисциплина"
+        verbose_name_plural = "Дисциплины"
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+#Посещаемость
+class Attendance(models.Model): 
+    date_time = models.DateTimeField(verbose_name="Дата и время")
+
+    student = models.ForeignKey("Student", on_delete=models.SET_NULL, null=True)
+    nagruzka = models.ForeignKey("Nagruzka", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Посещаемость"
+        verbose_name_plural = "Посещаемости"
+
+    def __str__(self) -> str:
+        return f"{self.date_time} {self.student} {self.nagruzka}"
 
 
+class Course_project(models.Model):
+    grade = models.IntegerField(verbose_name="Оценка")
+
+    student = models.ForeignKey("Student", on_delete=models.SET_NULL, null=True)
+    hps = models.ForeignKey("Hours_per_semestr", on_delete=models.SET_NULL, null=True) #В БД hours_per_semest
+
+    class Meta:
+        verbose_name = "Курсавая работа/проект"
+        verbose_name_plural = "Курсавые работы/проекты"
+
+    def __str__(self) -> str:
+        return f"{self.grade} {self.student} {self.hps}"
+    
+class Diploma(models.Model): # То ли диплом, то ли оценка за дисциплину
+    grade = models.IntegerField(verbose_name="Оценка")
+
+    student = models.ForeignKey("Student", on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
+    plan = models.ForeignKey("Plan", on_delete=models.SET_NULL, null=True)  
+
+    class Meta:
+        verbose_name = "Диплом?"
+        verbose_name_plural = "Дипломы?"
+
+    def __str__(self) -> str:
+        return f"{self.grade} {self.student} {self.teacher} {self.plan}"
+
+class Education_plan(models.Model):
+    code = models.TextField(max_length=255, verbose_name="Код")
+    year_of_conclude = models.IntegerField(verbose_name="Год завершения")
+
+    class Meta:
+        verbose_name = "Учебный план"
+        verbose_name_plural = "Учебные планы"
+
+    def __str__(self) -> str:
+        return f"{self.code} {self.year_of_conclude}"
+    
+class Form_control(models.Model):
+    form = models.TextField(max_length=255, verbose_name="Форма")
+    
+    hps = models.ForeignKey("Hours_per_semestr", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Форма контроля"
+        verbose_name_plural = "Формы контроля"
+
+    def __str__(self) -> str:
+        return f"{self.form} {self.hps}"
+    
+class Grade(models.Model):
+    grade = models.IntegerField(verbose_name="Оценка")
+    
+    student = models.ForeignKey("Student", on_delete=models.SET_NULL, null=True)
+    form_control = models.ForeignKey("Form_control", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Оценка"
+        verbose_name_plural = "Оценки"
+
+    def __str__(self) -> str:
+        return f"{self.grade} {self.student} {self.form_control}"
+    
+class Hours_per_semestr(models.Model):
+    hours = models.IntegerField(verbose_name="Часы")
+    semester = models.IntegerField(verbose_name="Семестр")
+    
+    plan = models.ForeignKey("Education_plan", on_delete=models.SET_NULL, null=True)
+    disciple = models.ForeignKey("Disciple", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Оценка"
+        verbose_name_plural = "Оценки"
+
+    def __str__(self) -> str:
+        return f"{self.hours} {self.semester} {self.plan} {self.disciple}"
+    
+class Nagruzka(models.Model):
+ 
+    teacher = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
+    group = models.ForeignKey("Group", on_delete=models.SET_NULL, null=True)
+    form_control = models.ForeignKey("Form_control", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Нагрузка"
+        verbose_name_plural = "Нагрузки"
+
+    def __str__(self) -> str:
+        return f"{self.teacher} {self.group} {self.form_control}"
+    
+class Practise(models.Model):
+
+    grade = models.IntegerField(verbose_name="Оценка")
+ 
+    student = models.ForeignKey("Student", on_delete=models.SET_NULL, null=True)
+    education_plan = models.ForeignKey("Education_plan", on_delete=models.SET_NULL, null=True)
+    practise_type = models.ForeignKey("Practise_type", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Практика"
+        verbose_name_plural = "Практики"
+
+    def __str__(self) -> str:
+        return f"{self.grade} {self.student} {self.education_plan} {self.practise_type}"
+    
+class Practise_type(models.Model):
+
+    title = models.TextField(max_length=255, verbose_name="Название типа практики")
+ 
+    class Meta:
+        verbose_name = "Тип практики"
+        verbose_name_plural = "Типы практики"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+    
+class Rating(models.Model):
+
+    event = models.TextField(max_length=255, verbose_name="Мероприятие")
+    score = models.IntegerField(verbose_name="Баллы")
+
+    rating_type = models.ForeignKey("Rating_type", on_delete=models.SET_NULL, null=True)
+ 
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+    def __str__(self) -> str:
+        return f"{self.event} {self.score} {self.rating_type}"
+
+class Rating_type(models.Model):
+
+    title = models.TextField(max_length=255, verbose_name="Название")
+ 
+    class Meta:
+        verbose_name = "Тип рейтинга"
+        verbose_name_plural = "Типы рейтинга"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+    
 class Speciality(models.Model):
-    code = models.TextField(max_length=30, verbose_name="Код")
-    name = models.TextField(max_length=30, verbose_name="Название специальности")
 
+    code = models.TextField(max_length=255, verbose_name="Код")
+    title = models.TextField(max_length=255, verbose_name="Название")
+ 
     class Meta:
         verbose_name = "Специальность"
         verbose_name_plural = "Специальности"
 
     def __str__(self) -> str:
-        return f"{self.code} {self.name}"
-
-
-class Subject(models.Model):
-    code = models.TextField(max_length=30, verbose_name="Код")
-    name = models.TextField(max_length=30, verbose_name="Название предмета")
-
-    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE, null=True)
-
-    class Meta:
-        verbose_name = "Предмет"
-        verbose_name_plural = "Предметы"
-
-    def __str__(self) -> str:
-        return f"{self.code} {self.name}"
-
-
-class Teacher_Subject(models.Model):
-    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE, null=True)
-    subject = models.ForeignKey("Subject", on_delete=models.CASCADE, null=True)
-
-    class Meta:
-        verbose_name = "Предмет преподавателя"
-        verbose_name_plural = "Предметы преподавателей"
-
-    def __str__(self) -> str:
-        return f"{self.code} {self.name}"
-
-
-class Student_Subject(models.Model):
-    grade = models.IntegerField(verbose_name="Оценка")
-
-    student = models.ForeignKey("Student", on_delete=models.CASCADE, null=True)
-    subject = models.ForeignKey("Subject", on_delete=models.CASCADE, null=True)
-
-    class Meta:
-        verbose_name = "Оценка студента"
-        verbose_name_plural = "Оценки студентов"
-
-    def __str__(self) -> str:
-        return f"{self.student} {self.subject} {self.grade}"
+        return f"{self.title} {self.code}"
