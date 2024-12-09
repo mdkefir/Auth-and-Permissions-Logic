619c7ecc4916596fe77a65d23c9b88e9e4387303
AshHvostovsky <ash.stdw@gmail.com>
2024-12-09 17:01:51 +0800

    Added 14 views

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from application.models import Group, Student, Teacher
+from application.models import Group, Student, Teacher, Disciple, Attendance, Course_project, Diploma, Education_plan, Form_control, Grade, Hours_per_semestr, Complexity, Practise, Practise_type, Rating, Rating_type, Speciality
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +16,88 @@ class StudentSerializer(serializers.ModelSerializer):
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
+        fields = '__all__'
+
+
+class DiscipleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disciple
+        fields = '__all__'
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = '__all__'
+
+
+class CourseProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course_project
+        fields = '__all__'
+
+
+class DiplomaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diploma
+        fields = '__all__'
+
+
+class EducationPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education_plan
+        fields = '__all__'
+
+
+class FormControlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Form_control
+        fields = '__all__'
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
+        fields = '__all__'
+
+
+class HoursPerSemestrSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hours_per_semestr
+        fields = '__all__'
+
+
+class ComplexitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Complexity
+        fields = '__all__'
+
+
+class PractiseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Practise
+        fields = '__all__'
+
+
+class PractiseTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Practise_type
+        fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
+class RatingTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating_type
+        fields = '__all__'
+
+
+class SpecialitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Speciality
         fields = '__all__'
--- application/views.py
+++ application/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Group, Student, Teacher
-from app.serializers import GroupSerializer, StudentSerializer, TeacherSerializer
+from .models import Group, Student, Teacher, Disciple, Attendance, Course_project, Diploma, Education_plan, Form_control, Grade, Hours_per_semestr, Complexity, Practise, Practise_type, Rating, Rating_type, Speciality
+from app.serializers import GroupSerializer, StudentSerializer, TeacherSerializer, DiscipleSerializer, AttendanceSerializer, CourseProjectSerializer, DiplomaSerializer, EducationPlanSerializer, FormControlSerializer, GradeSerializer, HoursPerSemestrSerializer, ComplexitySerializer, PractiseSerializer, PractiseTypeSerializer, RatingSerializer, RatingTypeSerializer, SpecialitySerializer
 
 
 class HelloWorldAPIView(APIView):
@@ -29,3 +29,102 @@ class TeacherListView(APIView):
         teachers = Teacher.objects.all()
         serializer = TeacherSerializer(teachers, many=True)
         return Response(serializer.data)
+    
+
+class DiscipleListView(APIView):
+    def get(self, request):
+        disciples = Disciple.objects.all()
+        serializer = DiscipleSerializer(disciples, many=True)
+        return Response(serializer.data)
+    
+
+class AttendanceListView(APIView):
+    def get(self, request):
+        attendances = Attendance.objects.all()
+        serializer = AttendanceSerializer(attendances, many=True)
+        return Response(serializer.data)
+    
+
+class CourseProjectListView(APIView):
+    def get(self, request):
+        course_projects = Course_project.objects.all()
+        serializer = CourseProjectSerializer(course_projects, many=True)
+        return Response(serializer.data)
+    
+
+class DiplomaListView(APIView):
+    def get(self, request):
+        diplomas = Diploma.objects.all()
+        serializer = DiplomaSerializer(diplomas, many=True)
+        return Response(serializer.data)
+    
+
+class EducationPlanListView(APIView):
+    def get(self, request):
+        education_plans = Education_plan.objects.all()
+        serializer = EducationPlanSerializer(education_plans, many=True)
+        return Response(serializer.data)
+    
+
+class FormControlListView(APIView):
+    def get(self, request):
+        form_controls = Form_control.objects.all()
+        serializer = FormControlSerializer(form_controls, many=True)
+        return Response(serializer.data)
+
+
+class GradeListView(APIView):
+    def get(self, request):
+        grades = Grade.objects.all()
+        serializer = GradeSerializer(grades, many=True)
+        return Response(serializer.data)
+
+
+class HoursPerSemestrListView(APIView):
+    def get(self, request):
+        hours_per_semesters = Hours_per_semestr.objects.all()
+        serializer = HoursPerSemestrSerializer(hours_per_semesters, many=True)
+        return Response(serializer.data)
+
+
+class ComplexityListView(APIView):
+    def get(self, request):
+        complexities = Complexity.objects.all()
+        serializer = ComplexitySerializer(complexities, many=True)
+        return Response(serializer.data)
+
+
+class PractiseListView(APIView):
+    def get(self, request):
+        practises = Practise.objects.all()
+        serializer = PractiseSerializer(practises, many=True)
+        return Response(serializer.data)
+
+
+class PractiseTypeListView(APIView):
+    def get(self, request):
+        practise_types = Practise_type.objects.all()
+        serializer = PractiseTypeSerializer(practise_types, many=True)
+        return Response(serializer.data)
+
+
+class RatingListView(APIView):
+    def get(self, request):
+        ratings = Rating.objects.all()
+        serializer = RatingSerializer(ratings, many=True)
+        return Response(serializer.data)
+
+
+class RatingTypeListView(APIView):
+    def get(self, request):
+        rating_types = Rating_type.objects.all()
+        serializer = RatingTypeSerializer(rating_types, many=True)
+        return Response(serializer.data)
+
+
+class SpecialityListView(APIView):
+    def get(self, request):
+        specialities = Speciality.objects.all()
+        serializer = SpecialitySerializer(specialities, many=True)
+        return Response(serializer.data)
+
