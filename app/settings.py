098e963c69081d223a7af34bce8f4881d62cc520
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-03-09 22:08:49 +0800

    CORS again

--- app/settings.py
+++ app/settings.py
@@ -61,6 +61,29 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # ! Важно указать именно ваш адрес где находится фронт, иначе не сработает
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
