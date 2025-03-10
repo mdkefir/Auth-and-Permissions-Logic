95eb2158858fa10861e7e93fbe2b5d00043a6bbe
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-03-10 11:14:23 +0800

    Временно для теста sqlite пока нет SQL сервера

--- app/settings.py
+++ app/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'IRNTU-Dashboard',
-        'USER': 'postgres',
-        'PASSWORD': 'rootroot',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
